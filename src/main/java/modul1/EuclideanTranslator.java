06b087bb07c3ef00eba485e059ed7e03c0c7c898
strus100 <danek130@gmail.com>
2020-11-06 21:36:02 +0100

    end module 1

---
+++ src/main/java/modul1/EuclideanTranslator.java
@@ -0,0 +1,30 @@
+package modul1;
+
+import java.math.BigInteger;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class EuclideanTranslator {
+
+    public BigInteger translate(BigInteger y, BigInteger lastElement) {
+        Map<BigInteger, BigInteger> mapTranslate = initMap(lastElement);
+
+       return mapTranslate.get(y);
+    }
+
+    public Map<BigInteger, BigInteger> initMap(BigInteger lastElement) {
+        LinkedList<BigInteger> keys = new LinkedList<BigInteger>();
+        LinkedList<BigInteger> values = new LinkedList<BigInteger>();
+        for (BigInteger i = BigInteger.ONE; i.compareTo(lastElement) <=0 ; i = i.add(BigInteger.ONE)) {
+            values.add(i.subtract(BigInteger.ONE));
+            keys.add(i.negate());
+        }
+        Collections.reverse(keys);
+
+       return IntStream.range(0, keys.size()).boxed()
+                .collect(Collectors.toMap(keys::get, values::get));
+    }
+}
---
+++ src/main/java/modul1/FermatTest.java
@@ -0,0 +1,14 @@
+package modul1;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+public class FermatTest {
+    public BigDecimal fermatTest(BigInteger b, BigInteger n){
+        return FastPower.bit_pow(
+                new BigDecimal( b ),
+                n.subtract( BigInteger.valueOf( 1 ) )
+        )
+                .remainder( new BigDecimal( n ) );
+    }
+}
