ef0fc04176e37938b6c1de679afa1782274c3fb9
Daniel Matuszewski <strus100@o2.pl>
2021-01-29 19:17:55 +0100

    module 4 done \n\n -test

--- src/main/java/module4/AES.java
+++ src/main/java/module4/AES.java
@@ -8,9 +8,15 @@ public class AES {
         return first^second;
     }
 
-    public int xtime(int first, int times ){
-        return first<<times;
+    public int xtime(int first ){
+        first = first<<1;
+        if(first > 255){
+            return modulo( first );
+        } else {
+            return first;
+        }
     }
+
 
     public int iloczyn(int first, int second ){
         String s = toString( second );
@@ -20,7 +26,7 @@ public class AES {
         int j = 0;
         for (int i = s.length() - 1; i >= 0; i--) {
             if( s.charAt( j ) == '1' ){
-               temps.add( xtime( first, i ) );
+               temps.add( manyXtime( first, i) );
             }
             j++;
         }
@@ -34,6 +40,15 @@ public class AES {
         return modulo( result );
     }
 
+    private int manyXtime(int first, int i) {
+        int temp = first;
+        for (int j = 0; j < i ; j++) {
+            temp = xtime( temp );
+        }
+        return temp;
+    }
+
+
     public int modulo(int result ){
         if( result > 255){
             result = toInt( result );
@@ -44,6 +59,7 @@ public class AES {
         }
     }
     
+
     public int odwrotnosc( int first ){
         for (int i = 1; i < 256 ; i++) {
             int temp = iloczyn(first, i);
@@ -53,6 +69,7 @@ public class AES {
         }
         return -1;
     }
+
 
     public String toString( int i ) {
        String string = Integer.toBinaryString(i);
@@ -76,4 +93,18 @@ public class AES {
     public int toInt( int i ){
         return Integer.parseInt( toString( i ), 2);
     }
+
+    public int fromHexToDec( String s ){
+        return Integer.parseInt( s, 16);
+    }
+
+    public int fromBinToDec( String s ){
+        return Integer.parseInt( s, 2);
+    }
+
+    public String fromDecToHex( int i ){
+       return Integer.toHexString(i).toUpperCase();
+    }
+
+
 }
--- src/test/java/module4/AESTest.java
+++ src/test/java/module4/AESTest.java
@@ -15,23 +15,32 @@ class AESTest {
     @Test
     void xtime() {
         AES aes = new AES();
-        assertEquals( 2, aes.xtime(1, 1) );
+        assertEquals( 2, aes.xtime(1 ) );
+
+        int x = aes.xtime(87);
+        x = aes.xtime(x);
+        x = aes.xtime(x);
+        x = aes.xtime(x);
+        assertEquals(x, 7);
+
     }
+
 
     @Test
     void iloczyn() {
         AES aes = new AES();
         assertEquals( 24, aes.iloczyn(4,6) );
-        System.out.println( aes.toString( aes.iloczyn(15,206) ) );
+        assertEquals( 7, aes.iloczyn(87,16) );
+        assertEquals( 1, aes.iloczyn(58,32) );
 
     }
 
     @Test
     void odwrotnosc() {
         AES aes = new AES();
-        System.out.println( aes.toString( aes.odwrotnosc( 14 ) ) );
-        System.out.println( aes.toString( 14 ) );
+        assertEquals(  58  ,aes.odwrotnosc( 32 )  );
     }
+
 
     @Test
     void testToString() {
@@ -43,5 +52,23 @@ class AESTest {
     void modulo(){
         AES aes = new AES();
         aes.modulo( 7);
+    }
+
+    @Test
+    void fromBitToDec(){
+        AES aes = new AES();
+        assertEquals( 58, aes.fromBinToDec("111010")) ;
+    }
+
+    @Test
+    void fromHexToDec(){
+        AES aes = new AES();
+        assertEquals( 58, aes.fromHexToDec("3A")) ;
+    }
+
+    @Test
+    void fromDecToHex(){
+        AES aes = new AES();
+        assertEquals( "3A", aes.fromDecToHex( 58 ) );
     }
 }
